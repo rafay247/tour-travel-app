eeced8af50e841324ce2bcd23de7a2d762da57c4
Rafay shaikh <rafayf3@gmail.com>
2023-10-09 23:22:03 +0500

    login registration

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,14 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import { Col, Container, Row, Form, FormGroup, Button } from 'reactstrap'
 import '../styles/login.css'
 
 import loginImg from '../assets/images/login.png'
 import userIcon from '../assets/images/user.png'
+
+import { AuthContextt } from '../context/AuthContext'
+import { BASE_URL } from '../utils/config'
+
 
 const Login = () => {
 
@@ -13,8 +17,32 @@ const Login = () => {
     password: undefined
 
   })
-  const handleClick = e => {
+  const { dispatch } = useContext(AuthContextt)
+  const navigate = useNavigate()
+
+  const handleClick = async e => {
     e.preventDefault()
+    dispatch({ type: 'LOGIN_START' })
+
+    try {
+      const res = await fetch(`${BASE_URL}/auth/login`, {
+        method: 'post',
+        headers: {
+          'content-type': 'application/json'
+        },
+        credentials: 'include',
+        body: JSON.stringify(credentials)
+      })
+      const result = await res.json()
+
+      if (!res.ok) alert(result.message)
+      dispatch({ type: 'LOGIN_SUCCESS', payload: result.data })
+      navigate('/')
+
+    } catch (error) {
+      dispatch({ type: 'LOGIN_FAILED', payload: error.message })
+      alert(error.message)
+    }
 
   }
 
@@ -27,7 +55,7 @@ const Login = () => {
       <Row>
         <Col lg='8' className='m-auto'>
           <div className='login__container d-flex justify-content-between'>
-            
+
             <div className='login__img'>
               <img src={loginImg} alt="" />
             </div>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,14 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import { Col, Container, Row, Form, FormGroup, Button } from 'reactstrap'
 import '../styles/login.css'
 
+
 import registerImg from '../assets/images/register.png'
 import userIcon from '../assets/images/user.png'
+
+import { AuthContextt } from '../context/AuthContext'
+import { BASE_URL } from '../utils/config'
 
 const Register = () => {
 
@@ -14,8 +18,29 @@ const Register = () => {
     password: undefined
 
   })
-  const handleClick = e => {
+  const { dispatch } = useContext(AuthContextt)
+  const navigate = useNavigate()
+
+  const handleClick = async e => {
     e.preventDefault()
+
+    try {
+      const res = await fetch(`${BASE_URL}/auth/register`, {
+        method: 'post',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+      })
+      const result = await res.json()
+
+      if (!res.ok) alert(result.message)
+      dispatch({ type: 'REGISTER_SUCCESS' })
+      navigate('/login')
+
+    } catch (error) {
+      alert(error.message)
+    }
 
   }
 
